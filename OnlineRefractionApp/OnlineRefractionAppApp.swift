6909a1270201ef98af9467c99d03e17059a9e0f0
DionysusLing <liuyingan812@gmail.com>
2025-07-29 19:46:23 +0800

    chore: init

---
+++ OnlineRefractionApp/OnlineRefractionAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnlineRefractionAppApp.swift
+//  OnlineRefractionApp
+//
+//  Created by dionysus on 2025/7/29.
+//
+
+import SwiftUI
+
+@main
+struct OnlineRefractionAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ OnlineRefractionAppTests/OnlineRefractionAppTests.swift
@@ -0,0 +1,16 @@
+//
+//  OnlineRefractionAppTests.swift
+//  OnlineRefractionAppTests
+//
+//  Created by dionysus on 2025/7/29.
+//
+
+import Testing
+
+struct OnlineRefractionAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
